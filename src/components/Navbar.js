8956bb684f1cd784e6f2950db0611ea82c876ecb
Prabhat <krsinghp8@gmail.com>
2024-04-30 04:38:38 -0400

    backroads pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import logo from '../images/logo.svg'
+import { pageLink, socialLinks } from '../data'
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        {/* <!-- left this comment on purpose --> */}
+        <ul className="nav-links" id="nav-links">
+          {pageLink.map(({ id, href, text }) => {
+            return (
+              <li key={id}>
+                <a href={href} className="nav-link">
+                  {text}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          {socialLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a
+                  href={link.href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="nav-icon"
+                >
+                  <i className={link.icon}></i>
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+export default Navbar
