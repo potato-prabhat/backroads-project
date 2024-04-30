a8190840b81de34b4649b83035a61ac6a82b5d73
Prabhat <krsinghp8@gmail.com>
2024-04-30 05:39:44 -0400

    More changes

--- src/data.js
+++ src/data.js
@@ -1,3 +1,7 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
 export const pageLink = [
   { id: 1, href: '#home', text: 'home' },
   { id: 2, href: '#about', text: 'about' },
@@ -8,4 +12,64 @@ export const socialLinks = [
   { id: 1, href: 'https://www.x.com', icon: 'fab fa-facebook' },
   { id: 2, href: 'https://www.x.com', icon: 'fab fa-twitter' },
   { id: 3, href: 'https://www.x.com', icon: 'fab fa-squarespace' },
+]
+export const services = [
+  {
+    id: 1,
+    title: 'saving money',
+    icon: 'fas fa-wallet fa-fw',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.',
+  },
+  {
+    id: 2,
+    title: 'endless hiking',
+    icon: 'fas fa-tree fa-fw',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.',
+  },
+  {
+    id: 3,
+    title: 'amazing comfort',
+    icon: 'fas fa-socks fa-fw',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.',
+  },
+]
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: 'august 26th, 2020',
+    info: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 6,
+    cost: '2100',
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    info: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 6,
+    cost: '2100',
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: 'august 26th, 2020',
+    info: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 6,
+    cost: '2100',
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: 'august 26th, 2020',
+    info: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 6,
+    cost: '2100',
+  },
 ]
